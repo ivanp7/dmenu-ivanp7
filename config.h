f29878dc6465bbac878a0aaf46735f4b9fa81572
ivanp7 <ivanp7@mail.ru>
2023-02-21 13:00:15 +0300

    apply lineheight patch

--- config.h
+++ config.h
@@ -22,7 +22,9 @@ static const char *colors[SchemeLast][2] = {
 static unsigned int lines      = 0;
 static unsigned int columns    = 0;
 
-static unsigned int lineheight = 0;         /* -h option; minimum height of a menu line     */
+/* -h option; minimum height of a menu line     */
+static unsigned int lineheight = 0;
+static unsigned int min_lineheight = 8;
 
 /*
  * Characters not considered part of a word while deleting words
